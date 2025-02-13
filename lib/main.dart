f2a5ab127ec70534fdc30664de7cbe6b2a35c28b
abigailsheldon <abigail.l.sheldon@gmail.com>
2025-02-12 19:23:06 -0500

    Adds animation

--- lib/main.dart
+++ lib/main.dart
@@ -28,19 +28,32 @@ class ConfettiScreen extends StatefulWidget{
 
 }
 
-class _ConfettiScreenState extends State<ConfettiScreen> {
+class _ConfettiScreenState extends State<ConfettiScreen> 
+    with SingleTickerProviderStateMixin{
   late ConfettiController _confettiController;
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
 
   @override
   void initState() {
     super.initState();
     _confettiController = ConfettiController(duration: const Duration(days:365));
     _confettiController.play();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 800),
+      )..repeat(reverse: true);
+      
+      _scaleAnimation = Tween<double>(begin: 1.0, end:1.2).animate(
+        CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
+      );
   }
 
   @override
   void dispose() {
     _confettiController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -71,12 +84,20 @@ class _ConfettiScreenState extends State<ConfettiScreen> {
                   ),
                   
                   SizedBox(height: 20),
-                  Image.asset(
-                    'assets/images/image2.png',
-                    fit: BoxFit.contain,
-                    width: double.infinity,
+                  ScaleTransition(
+                  scale: _scaleAnimation,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.8,
                     height: 300,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
                     ),
+                    child: Image.asset(
+                      'assets/images/image2.png',
+                      fit: BoxFit.fitWidth,
+                    ),
+                  ),
+                  ),
                 ],
             ),
           ),
